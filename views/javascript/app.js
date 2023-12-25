9fd7ab3da26e00bd1100098283d4a3c6c47c2d96
izasoerya <ihza.surya@gmail.com>
2023-12-25 15:00:29 +0700

    button and text working fine

--- views/javascript/app.js
+++ views/javascript/app.js
@@ -1,14 +1,25 @@
+// await contentLoaded
 document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("input-type")
-    .addEventListener("submit", function (event) {
-      event.preventDefault(); // Prevent form submission
-
-      var buttons = document.getElementsByClassName("button-4");
-      for (let index = 0; index < buttons.length; index++) {
-        buttons[index].addEventListener("click", function (event) {
-          alert(buttons[index].textContent);
-        });
-      }
+  var buttons = document.getElementsByClassName("button-4");
+  for (let index = 0; index < buttons.length; index++) {
+    buttons[index].addEventListener("click", function (event) {
+      event.preventDefault();
+      fetchRequest();
     });
+  }
 });
+
+function fetchRequest() {
+  var textUser = document.getElementsByClassName("input-user");
+  console.log(textUser[0].value);
+  // fetch(`localhost:3000/${buttons[index].textContent}`),
+  //   {
+  //     method: "GET",
+  //     body: JSON.stringify({
+  //       title: inputText.textContent,
+  //     }),
+  //     Headers: {
+  //       "content-type": "application/json; charset=utf-8",
+  //     },
+  //   };
+}
